8f87b93bf323e9e5851257b593b2f6f17ee8572b
HarshAProgrammer <App.Lavishly@Gmail.com>
2024-01-21 20:04:40 +0530

    Working successfully

--- app/src/main/java/com/example/passingdataexplicitintent/HomeActivity.kt
+++ app/src/main/java/com/example/passingdataexplicitintent/HomeActivity.kt
@@ -1,23 +1,44 @@
 package com.example.passingdataexplicitintent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 
 class HomeActivity : AppCompatActivity() {
+
+    // creating key
+    companion object{
+
+    const val KEY = "com.example.passingdataexplicitintent.HomeActivity.KEY"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+
+
         val first = findViewById<EditText>(R.id.firstOrder)
         val second = findViewById<EditText>(R.id.secondOrder)
+        val third = findViewById<EditText>(R.id.thirdOrder)
+        val fourth = findViewById<EditText>(R.id.fourthOrder)
         val button = findViewById<Button>(R.id.btnSend)
 
         button.setOnClickListener {
+
+            val message = first.text.toString() + " " +second.text.toString() + " " +third.text.toString() + " " +
+                    fourth.text.toString()
+
+            val intent = Intent(applicationContext,SecondActivity::class.java)
+            intent.putExtra("Order",message)
+            startActivity(intent)
+
 
         }
 
 
     }
 }
+
--- app/src/main/java/com/example/passingdataexplicitintent/SecondActivity.kt
+++ app/src/main/java/com/example/passingdataexplicitintent/SecondActivity.kt
@@ -2,10 +2,22 @@ package com.example.passingdataexplicitintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Contacts.SettingsColumns.KEY
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val order = findViewById<TextView>(R.id.receivedOrder)
+
+        val orderOfCustomers = intent.getStringExtra("Order")
+
+        order.text = orderOfCustomers.toString()
+
+
+
+
     }
 }
