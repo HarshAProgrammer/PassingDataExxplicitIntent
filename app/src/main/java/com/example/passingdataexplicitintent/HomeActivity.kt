d6661e3b1c70e21fe190b08c6233b8291b76791f
HarshAProgrammer <App.Lavishly@Gmail.com>
2024-01-21 18:22:29 +0530

    Second Commit

--- app/src/main/java/com/example/passingdataexplicitintent/HomeActivity.kt
+++ app/src/main/java/com/example/passingdataexplicitintent/HomeActivity.kt
@@ -2,10 +2,22 @@ package com.example.passingdataexplicitintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val first = findViewById<EditText>(R.id.firstOrder)
+        val second = findViewById<EditText>(R.id.secondOrder)
+        val button = findViewById<Button>(R.id.btnSend)
+
+        button.setOnClickListener {
+
+        }
+
+
     }
 }
